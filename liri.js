9b79de371b15b506ac5aff6668be6fae37392dad
Stephen Doe <redcard1016@yahoo.com>
2016-07-11 00:14:25 -0400

    Nothing to see here

--- liri.js
+++ liri.js
@@ -1,2 +1,14 @@
-var twitterKeys = require('./keys.js');
+var twitterKeys = require('./twitter.js');
 var Twitter = require('twitter');
+
+var params = {
+  screen_name: 'MrStephenDoe'
+};
+
+client.get('statuses/user_timeline', params);
+
+ function mytweets(error, tweets, response){
+  if (!error) {
+    console.log(tweets);
+  }
+};
